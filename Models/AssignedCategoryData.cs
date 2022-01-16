88e053c1ac6be3fd261194204e3d061506032124
Georgian Preda <predageorgian8@gmail.com>
2022-01-16 23:02:27 +0200

    Fix typos

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,9 @@
+﻿namespace Preda_Georgian_Lab8.Models
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
